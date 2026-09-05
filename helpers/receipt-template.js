02c9182671983a6f773b693833da8c62fe3d4d02
RKhannaRaj <rkhannaraj@gmail.com>
2026-09-05 10:00:18 +0100

    Receipt PDF refund-2

--- helpers/receipt-template.js
+++ helpers/receipt-template.js
@@ -63,6 +63,9 @@ async function generatePaymentReceiptPdf(req, res) {
 
       const isRefund = payment.paymentType === "REFUND";
 
+      console.log("Receipt Payment.PaymentType", payment.paymentType);
+      console.log("Receipt isRefund", isRefund);
+
       if (p > 0) {
         doc.addPage();
         pageNumber++;
